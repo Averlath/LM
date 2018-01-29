af6e19e4d693dad0693c8e79b18d2aa8c9222f48
Ivan Penev <ivan.penev92@gmail.com>
2018-01-29 02:04:19 +0100

    Cambio de nombre de Examen LM

--- Examen LM/Javascript/Examen.js
+++
@@ -1,8 +0,0 @@
-function show() {
-    var x = document.getElementById("query");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
