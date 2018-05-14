fafb0fa72a932ceac14616841a3baa8f2ad3ea7e
Ivan Penev <ivan.penev92@gmail.com>
2018-05-14 17:08:32 +0200

    Indentation Fixed

--- CambridgeLevelExam/Javascript/javascript.js
+++ CambridgeLevelExam/Javascript/javascript.js
@@ -3,12 +3,12 @@ var unanswered=0;
 var level=null;
 
 window.onload=function() {
-	XML();
-	document.getElementById("mark").style.display = "none";
-	document.getElementById("sure").style.display = "none";
-	document.getElementById("form").style.display = "none";
-	document.getElementById("clock").style.display = "none";
-	document.getElementById("visualize").style.display = "none";
+    XML();
+    document.getElementById("mark").style.display = "none";
+    document.getElementById("sure").style.display = "none";
+    document.getElementById("form").style.display = "none";
+    document.getElementById("clock").style.display = "none";
+    document.getElementById("visualize").style.display = "none";
 }
 
 function XML() {
@@ -25,83 +25,83 @@ function XML() {
 }
 
 function submitinfo(obj) {
-	if (document.getElementById("textname").value == "") {
-		alert("Please write your name.");
-	} else if (document.getElementById("textlast").value == "") {
-		alert("Please write your last name.");
-	} else if (document.getElementById("address").value == "") {
-		alert("Please write you E-mail Address.");
-	} else {
-		document.getElementById("identification").style.display = "none";
-		document.getElementById("form").style.display = "block";
-		document.getElementById("clock").style.display = "block";
-		startTimer();
-	}
+    if (document.getElementById("textname").value == "") {
+	alert("Please write your name.");
+    } else if (document.getElementById("textlast").value == "") {
+	alert("Please write your last name.");
+    } else if (document.getElementById("address").value == "") {
+	alert("Please write you E-mail Address.");
+    } else {
+	document.getElementById("identification").style.display = "none";
+	document.getElementById("form").style.display = "block";
+	document.getElementById("clock").style.display = "block";
+	startTimer();
+    }
 }
 
 function clearinfo(obj) {
-	document.getElementById("textname").value = "";
-	document.getElementById("textlast").value = "";
-	document.getElementById("address").value = "";
+    document.getElementById("textname").value = "";
+    document.getElementById("textlast").value = "";
+    document.getElementById("address").value = "";
 }
 
 function startTimer() {
-	var minute = 14;
-	var second = 59;
+    var minute = 14;
+    var second = 59;
 
-	setInterval(function() {
-		if (second < 10) {
-			document.getElementById("timer").innerHTML = "00:" + minute + ":0" + second;
-		} else {
-			document.getElementById("timer").innerHTML = "00:" + minute + ":" + second;
-		}
-		if (second == 00) {
-		  	minute--;
-			second = 59;
-		}
-		if (minute == 0 && second == 1) {
-			EndExam();
-		}
-		second--;
-	}, 1000);
+    setInterval(function() {
+        if (second < 10) {
+	    document.getElementById("timer").innerHTML = "00:" + minute + ":0" + second;
+        } else {
+	    document.getElementById("timer").innerHTML = "00:" + minute + ":" + second;
+        }
+        if (second == 00) {
+	    minute--;
+	    second = 59;
+        }
+        if (minute == 0 && second == 1) {
+	    EndExam();
+        }
+        second--;
+    }, 1000);
 }
 
 function EndExam() {
-	document.getElementById("visualize").style.display = "block";
-	document.getElementById("clock").style.display = "none";
-	readOnly();
-	alert("Time is up.");
+    document.getElementById("visualize").style.display = "block";
+    document.getElementById("clock").style.display = "none";
+    readOnly();
+    alert("Time is up.");
 }
 
 function submit(obj) {
-	EndTest();
+    EndTest();
 }
 
 function EndTest() {
-	document.getElementById("button").style.display = "none";
-	document.getElementById("sure").style.display = "block";
+    document.getElementById("button").style.display = "none";
+    document.getElementById("sure").style.display = "block";
 }
 
 function submityes(obj) {
-	document.getElementById("visualize").style.display = "block";
-	document.getElementById("clock").style.display = "none";
-	checkCall();
-	readOnly();
+    document.getElementById("visualize").style.display = "block";
+    document.getElementById("clock").style.display = "none";
+    checkCall();
+    readOnly();
 }
 
 function submitno (obj) {
-	document.getElementById("button").style.display = "block";
-	document.getElementById("sure").style.display = "none";
+    document.getElementById("button").style.display = "block";
+    document.getElementById("sure").style.display = "none";
 }
 
 function visualize(obj) {
-	document.getElementById("visualize").style.display = "none";
-	document.getElementById("mark").style.display = "block";
-	totalPoints();
+    document.getElementById("visualize").style.display = "none";
+    document.getElementById("mark").style.display = "block";
+    totalPoints();
 }
 
 function readOnly() {
-	document.getElementById("fieldset").disabled = true;
+    document.getElementById("fieldset").disabled = true;
 }
 
 function Questions() {
