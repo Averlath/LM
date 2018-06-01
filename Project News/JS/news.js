8e7a496180a25b6a3309215ca1f37d1cd389beb5
Ivan Penev <ivan.penev92@gmail.com>
2018-06-01 19:02:46 +0200

    stuff

--- Project News/JS/news.js
+++ Project News/JS/news.js
@@ -1,13 +1,22 @@
 var JSON1;
 var JSON2;
 
+//Funcion para que cuando bajamos hasta la parte más baja de la página, cargue el JSON
+/*window.onscroll = function() {
+	if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+		alert('hi');
+		show(JSON1);
+	}
+};*/
+
+//Funcion AJAX para leer el JSON.
 $.ajax ({
 	url: 'https://raw.githubusercontent.com/Averlath/LM/master/Project%20News/data/1.json',
 	dataType: 'json',
 	type: 'get',
-	cache: false,
 	success: function(data) {
 		$(data.news).each(function(index, value) {
+			JSON1 = data;
 			console.log('Title:' + ' ' + value.Title);
 			console.log('ID: ' + value.ID);
 		});
@@ -18,7 +27,26 @@ $(window).on("scroll", function() {
   var scrollHeight = $(document).height();
   var scrollPosition = $(window).height() + $(window).scrollTop();
   if ((scrollHeight - scrollPosition) / scrollHeight === 0) {
-    visualize(JSON1);
-    visualize(NOTICIA2);
+    show(JSON1);
   }
 });
+
+function show(data) {
+  $.each( data, function(i, news) {
+    $("#see").append( "<h1 id='" + i + "'>" + news.Title+ "</h1>" + "<p>" + news.Date + "</p>" + "<img>" + news.ImageBig + "</img>" );
+  }); 
+}
+
+/*function show(jsonObject) {
+	$.each(jsonObject, function(i, news) {
+		var news = document.getElementById('news');
+		var div = document.createElement('div');
+		div.setAttribute('class');
+		news.appendChild(div)
+	});
+}
+
+function show() {
+	var div = document.createElement('div');
+	div.setAttribute()
+}*/
