1974b1c71ef53d4a596d4ba912470f4e202f63b1
Ivan Penev <ivan.penev92@gmail.com>
2018-06-01 19:58:14 +0200

    Reading and writing JSON in Chrome

--- Project News/JS/news.js
+++ Project News/JS/news.js
@@ -2,51 +2,33 @@ var JSON1;
 var JSON2;
 
 //Funcion para que cuando bajamos hasta la parte más baja de la página, cargue el JSON
-/*window.onscroll = function() {
+window.onscroll = function() {
 	if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-		alert('hi');
 		show(JSON1);
 	}
-};*/
+};
 
 //Funcion AJAX para leer el JSON.
-$.ajax ({
-	url: 'https://raw.githubusercontent.com/Averlath/LM/master/Project%20News/data/1.json',
-	dataType: 'json',
-	type: 'get',
-	success: function(data) {
-		$(data.news).each(function(index, value) {
-			JSON1 = data;
-			console.log('Title:' + ' ' + value.Title);
-			console.log('ID: ' + value.ID);
-		});
-	}
-});
-
-$(window).on("scroll", function() {
-  var scrollHeight = $(document).height();
-  var scrollPosition = $(window).height() + $(window).scrollTop();
-  if ((scrollHeight - scrollPosition) / scrollHeight === 0) {
-    show(JSON1);
-  }
-});
-
-function show(data) {
-  $.each( data, function(i, news) {
-    $("#see").append( "<h1 id='" + i + "'>" + news.Title+ "</h1>" + "<p>" + news.Date + "</p>" + "<img>" + news.ImageBig + "</img>" );
-  }); 
-}
-
-/*function show(jsonObject) {
-	$.each(jsonObject, function(i, news) {
-		var news = document.getElementById('news');
-		var div = document.createElement('div');
-		div.setAttribute('class');
-		news.appendChild(div)
+window.onload = function() {
+	$.ajax ({
+		url: 'https://raw.githubusercontent.com/Averlath/LM/master/Project%20News/data/1.json',
+		dataType: 'json',
+		type: 'get',
+		success: function(data) {
+			$(data.news).each(function(index, value) {
+				JSON1 = data["news"];
+				console.log('Title:' + ' ' + value.Title);
+				console.log('ID: ' + value.ID);
+			});
+		},
+		error: function() {
+			console.log("Error reading JSON");
+		}
 	});
 }
 
-function show() {
-	var div = document.createElement('div');
-	div.setAttribute()
-}*/
+function show(data) {
+	$(data).each(function(i, news) {
+		$("#see").append("<h1 id='" + i + "'>" + news.Title + "</h1>");
+	}); 
+}
