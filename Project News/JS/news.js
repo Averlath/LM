83bbf99426ca3b24a2ff3b97994911020d758b86
Ivan Penev <ivan.penev92@gmail.com>
2018-06-02 15:58:28 +0200

    news in html updated, some style fixes

--- Project News/JS/news.js
+++ Project News/JS/news.js
@@ -1,15 +1,23 @@
-//Variables Globales.
+//Global variables
 var JSON1;
 var JSON2;
 
-//otras variables
+//Other variables
 var newsAddedJson1=0;
 var newsTotalJson1=0;
 var newsAddedJson2=0;
 var newsTotalJson2=0;
 
-//Funcion AJAX para leer los JSON.
 window.onload = function() {
+	//Hidden text
+	document.getElementById("long").style.display = "none";
+	document.getElementById("long2").style.display = "none";
+	document.getElementById("long3").style.display = "none";
+	document.getElementById("show_less").style.display = "none";
+	document.getElementById("show_less2").style.display = "none";
+	document.getElementById("show_less3").style.display = "none";
+
+	//Ajax function to read JSON1.
 	$.ajax ({
 		url: 'https://raw.githubusercontent.com/Averlath/LM/master/Project%20News/data/1.json',
 		dataType: 'json',
@@ -25,6 +33,7 @@ window.onload = function() {
 		}
 	});
 
+	//Ajax function to read JSON2.
 	$.ajax ({
 		url: "https://raw.githubusercontent.com/Averlath/LM/master/Project%20News/data/2.json",
 		dataType: 'json',
@@ -35,14 +44,14 @@ window.onload = function() {
 				JSON2 = data2["news"];
 			});
 		}
-		//Error en cargar - Uncaught SyntaxError: Unexpected identifier
+		//Error when loading page - Uncaught SyntaxError: Unexpected identifier
 		/*error: function() {
 			console.log("Error reading JSON2");
 		}*/
 	});
 }
 
-//Funcion para que cuando bajamos hasta la parte más baja de la página, cargue el JSON
+//Function that loads next JSON whenever scrolling to Bottom of page
 window.onscroll = function() {
 	if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
 		if (newsTotalJson1 > newsAddedJson1) {
@@ -100,6 +109,7 @@ function createElementsJson2() {
     noMoreNews();
 }
 
+//Div that displays a paragraph, saying No More news at this time
 function noMoreNews() {
 	var section = document.getElementById("json_news2");
 
@@ -113,21 +123,58 @@ function noMoreNews() {
 	noNews.appendChild(noMore);
 }
 
-//Funcion Show, es la que da formato al JSON en HTML.
+//Funtion Show, writing and formatting JSON.
 function show(data) {
 	$(data).each(function(i, json) {
 		if (data==JSON1) {
 			$("#div3-json1").append("<div style='width: 720px; padding: 15px; margin: 20px 0px 10px 0px; border: 1px solid;' id='" + ("div" + 1+i + "'>"));
-			$("#div" + 1+i).append("<h1 class='text-info' style='font-size: 32px; text-align: justify' id='" + 1+i + "'>" + json.Title + "</h1>");
-			$("#div" + 1+i).append("<p>" + json.Date + "</p>");
+			$("#div" + 1+i).append("<h1 class='text-info' style='font-size: 22px; text-align: center; padding: 5px; id='" + 1+i + "'>" + json.Title + "</h1>");
+			$("#div" + 1+i).append("<p style='margin-left: 2%'>" + json.Date + "</p>");
 			$("#div" + 1+i).append("<p class='text-dark' style='text-align: justify;'><img src=" + json.ImageMid + " align='left' style='width: 300px; height: 200px; padding: 10px;' alt='json image' />" + json.News + "</p>");
 			newsAddedJson1++;
 		} else {
 			$("#div3-json2").append("<div style='width: 720px; padding: 15px; margin: 20px 0px 10px 0px; border: 1px solid;' id='" + ("div" + 2+i + "'>"));
-			$("#div" + 2+i).append("<h1 class='text-info' style='font-size: 32px; text-align: justify' id='" + 2+i + "'>" + json.Title + "</h1>");
-			$("#div" + 2+i).append("<p>" + json.Date + "</p>");
+			$("#div" + 2+i).append("<h1 class='text-info' style='font-size: 22px; text-align: center; padding: 5px; id='" + 2+i + "'>" + json.Title + "</h1>");
+			$("#div" + 2+i).append("<p style='margin-left: 2%'>" + json.Date + "</p>");
 			$("#div" + 2+i).append("<p class='text-dark' style='text-align: justify;'><img src=" + json.ImageMid + " align='left' style='width: 300px; height: 200px; padding: 10px;' alt='json image' />" + json.News + "</p>");
 			newsAddedJson2++;
 		}
 	});
+}
+
+//On click, show the remaining text of news 1, 2 and 3
+function show_news(obj) {
+	document.getElementById("long").style.display = "block";
+	document.getElementById("read_more").style.display = "none";
+	document.getElementById("show_less").style.display = "block";
+}
+
+function show_less(obj) {
+	document.getElementById("long").style.display = "none";
+	document.getElementById("read_more").style.display = "block";
+	document.getElementById("show_less").style.display = "none";
+}
+
+function show_news2(obj) {
+	document.getElementById("long2").style.display = "block";
+	document.getElementById("read_more2").style.display = "none";
+	document.getElementById("show_less2").style.display = "block";
+}
+
+function show_less2(obj) {
+	document.getElementById("long2").style.display = "none";
+	document.getElementById("read_more2").style.display = "block";
+	document.getElementById("show_less2").style.display = "none";
+}
+
+function show_news3(obj) {
+	document.getElementById("long3").style.display = "block";
+	document.getElementById("read_more3").style.display = "none";
+	document.getElementById("show_less3").style.display = "block";
+}
+
+function show_less3(obj) {
+	document.getElementById("long3").style.display = "none";
+	document.getElementById("read_more3").style.display = "block";
+	document.getElementById("show_less3").style.display = "none";
 }
